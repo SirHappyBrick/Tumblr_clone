7ba3448b48b85f27203c5fe34a7c79af91f093e2
Oskar <oskar.madej.wroc@gmail.com>
2021-10-14 00:15:24 +0200

    post controller and views

---
+++ db/schema.rb
@@ -0,0 +1,10 @@
+ActiveRecord::Schema.define(version: 2021_10_13_213429) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
